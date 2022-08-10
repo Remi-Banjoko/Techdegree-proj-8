66d0a941e8529d32f606ff56a5e09f70924b28a9
Remi-Banjoko <medlyrem@gmail.com>
2022-08-10 10:52:31 -0500

    working on modal prev

---
+++ app.js
@@ -0,0 +1,158 @@
+let employees = [];
+const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
+email, location, phone, dob &noinfo &nat=US`
+const gridContainer = document.querySelector(".grid-container");
+const overlay = document.querySelector(".overlay");
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
+const modalPrev = document.querySelector(".prev");
+const modalNext = document.querySelector(".next");
+
+fetch(urlAPI)
+.then(res => res.json())
+.then(res => res.results)
+.then(displayEmployees)
+.catch(err => console.log(err))
+
+
+
+function displayEmployees(employeeData) {
+    employees = employeeData;
+    // store the employee HTML as we create it
+    let employeeHTML = '';
+    // loop through each employee and create HTML markup
+    employees.forEach((employee, index) => {
+    let name = employee.name;
+    let email = employee.email;
+    let city = employee.location.city;
+    let picture = employee.picture;
+    // template literals make this so much cleaner!
+    employeeHTML += `
+    <div class="card" data-index="${index}">
+    <img class="avatar" src="${picture.large}" />
+    <div class="text-container">
+    <h2 class="name">${name.first} ${name.last}</h2>
+    <p class="email">${email}</p>
+    <p class="address">${city}</p>
+    </div>
+    </div>
+    `
+    });
+    gridContainer.innerHTML = employeeHTML;
+}
+
+function displayModal(index) {
+    // use object destructuring make our template literal cleaner
+    let { name, dob, phone, email, location: { city, street, state, postcode
+    }, picture } = employees[index];
+    let date = new Date(dob.date);
+    const modalHTML = `
+    <img class="avatar" src="${picture.large}" />
+    <div class="text-container">
+    <h2 class="name">${name.first} ${name.last}</h2>
+    <p class="email">${email}</p>
+    <p class="address">${city}</p>
+    <hr />
+    <p>${phone}</p>
+    <p class="address">${street}, ${state} ${postcode}</p>
+    <p>Birthday:
+    ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+    <div class= "modal-nav" >
+        <p class = "prev" > <--- Prev </p>
+        <p class = "next" > Next ---> </p>
+    </div>
+    </div>
+    `;
+    overlay.classList.remove("hidden");
+    modalContainer.innerHTML = modalHTML;
+    }
+
+    gridContainer.addEventListener('click', e => {
+        // make sure the click is not on the gridContainer itself
+
+        if(e.target !== gridContainer) {
+        
+            // select the card element based on its proximity to actual element clicked
+       
+        const card = e.target.closest(".card");
+        const index = card.getAttribute('data-index');
+        displayModal(index);
+        }
+        
+        });
+
+
+        modalPrev.addEventListener('click', () => {
+
+            console.log("HEYYYYYY");
+            const card = e.target.closest(".card");
+            const index = card.getAttribute('data-index');
+            overlay.classList.remove("hidden");
+            // displayModal(index-1);
+
+
+        });
+
+
+        modalClose.addEventListener('click', () => {
+            overlay.classList.add("hidden");
+            });
+
+
+
+
+             document.getElementById("search").addEventListener("keyup", myFunction);
+
+            // function myFunction() {
+
+            //     var user_input = document.getElementById("search").value;
+            //     var filter =  user_input.toUpperCase();
+            //     // var names = document.getElementsByClassName('name');
+            //     var names = document.querySelectorAll('.name');
+            //     var people = document.querySelectorAll('.card');
+
+            //     console.log(names);
+            //     console.log(people);
+            //     console.log(employees);
+
+            //     for (var i = 0; i < names.length; i++) {
+            //         if(names[i].textContent.toUpperCase().includes(filter)) {
+            //             names[i].classList.remove("hidden");
+            //         } else {
+            //             people[i].classList.add("hidden");
+            //         }
+            //     }
+
+
+            //   }
+
+
+
+
+            function myFunction() {
+                var user_input = document.getElementById("search").value;
+                var filter =  user_input.toUpperCase();
+                // var  = document.getElementById("image_gallery");
+                var gallery_entries = gallery.getElementsByClassName('name');
+              //   var gallery_captions = li.getAttribute("data-caption");
+              
+              
+              
+                for( let i = 0; i < gallery_entries.length; i++){
+              
+                  var a = gallery_entries[i].getElementsByTagName("a")[0];
+                  console.log(a.getAttribute("data-caption"));
+                  var txtValue = a.getAttribute("data-caption") || a.getAttribute("alt");
+                  if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                      gallery_entries[i].style.display = "list-item";
+                  } else {
+                      gallery_entries[i].style.display = "none";
+                  }
+              
+                  // let caption = image_cap`${i}`.getAttribute("data-caption");
+                  // console.log(caption);
+                  
+                  }
+                  
+              
+              }
